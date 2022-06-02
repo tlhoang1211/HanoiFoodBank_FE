fd4d0ac74ee85d79ebd6a365eacdd1f6d1e03f2e
Hoàng Trần <46571690+tlhoang1211@users.noreply.github.com>
2022-06-02 18:52:22 +0700

    1

--- assets/js/notification.js
+++ assets/js/notification.js
@@ -1,35 +1,50 @@
 var firebase;
 var Notification = {
-    config: function () {
-        const firebaseConfig = {
-            apiKey: "AIzaSyA7bO12wekdhrlDxHxKCkgfg-a2PvfIG3c",
-            authDomain: "demofcm-c0caa.firebaseapp.com",
-            databaseURL: "https://demofcm-c0caa-default-rtdb.firebaseio.com",
-            projectId: "demofcm-c0caa",
-            storageBucket: "demofcm-c0caa.appspot.com",
-            messagingSenderId: "16196139174",
-            appId: "1:16196139174:web:c852e2fbfcd905a2db5c67"
-        };
-        if (!firebase.apps.length) {
-            firebase.initializeApp(firebaseConfig);
-         }else {
-            firebase.app(); // if already initialized, use that one
-         }
-        // firebase.initializeApp(firebaseConfig);
-    },
-    send: function (userId, notify) {
-        firebase.database().ref(`notification/${userId}`).push().set(notify);
-    },
-    show: function (userId, callback) {
-        var db = firebase.database();
-        db.ref(`notification/${userId}`).on("value", function (snapshot) {
-            callback(snapshot)
-        });
-    },
-    update: function (userId, idNotify, notify) {
-        firebase.database().ref().child(`notification/${userId}/${idNotify}`).update(notify);
-    },
-    delete: function (userId, idNotify, notify) {
-        firebase.database().ref().child(`notification/${userId}/${idNotify}`).remove();
+  config: function () {
+    const firebaseConfig = {
+      // apiKey: "AIzaSyA7bO12wekdhrlDxHxKCkgfg-a2PvfIG3c",
+      // authDomain: "demofcm-c0caa.firebaseapp.com",
+      // databaseURL: "https://demofcm-c0caa-default-rtdb.firebaseio.com",
+      // projectId: "demofcm-c0caa",
+      // storageBucket: "demofcm-c0caa.appspot.com",
+      // messagingSenderId: "16196139174",
+      // appId: "1:16196139174:web:c852e2fbfcd905a2db5c67"
+      apiKey: "AIzaSyBUO9xOl0ZFw6XbH72d4iTTICCXZzPpvJo",
+      authDomain: "hanoifoodbank.firebaseapp.com",
+      projectId: "hanoifoodbank",
+      storageBucket: "hanoifoodbank.appspot.com",
+      messagingSenderId: "306648236869",
+      appId: "1:306648236869:web:465c86ac6aa7ffed1df42c",
+      measurementId: "G-EKDV68NTRE",
+    };
+    if (!firebase.apps.length) {
+      firebase.initializeApp(firebaseConfig);
+    } else {
+      firebase.app(); // if already initialized, use that one
     }
-}
+    // firebase.initializeApp(firebaseConfig);
+  },
+  send: function (userId, notify) {
+    firebase.database().ref(`notification/${userId}`).push().set(notify);
+  },
+  show: function (userId, callback) {
+    var db = firebase.database();
+    db.ref(`notification/${userId}`).on("value", function (snapshot) {
+      callback(snapshot);
+    });
+  },
+  update: function (userId, idNotify, notify) {
+    firebase
+      .database()
+      .ref()
+      .child(`notification/${userId}/${idNotify}`)
+      .update(notify);
+  },
+  delete: function (userId, idNotify, notify) {
+    firebase
+      .database()
+      .ref()
+      .child(`notification/${userId}/${idNotify}`)
+      .remove();
+  },
+};
